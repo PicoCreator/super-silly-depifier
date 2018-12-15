2a73628493a0df4a66085ec383da4e11b2c0e2ff
Fazli Sapuan <fazli@sapuan.org>
2018-12-15 13:47:43 +0800

    fasfasf

--- index.js
+++ index.js
@@ -6,7 +6,25 @@ filename = process.argv[2];
 js = fs.readFileSync(filename, 'utf-8');
 ast = Parser.parse(js);
 
-console.log(ast);
+//console.log(ast);
+
+
+
+function stmtTolabel(ast, stmt) {
+  const label = stmt.join('')
+    .replace(new RegExp(' ', 'g'), '_')
+    .replace(new RegExp(';', 'g'), '')
+    .replace(new RegExp('\n', 'g'), '')
+    .replace(new RegExp('=', 'g'), 'equals')
+    .replace(new RegExp('\\+', 'g'), 'plus')
+    .replace(new RegExp('-', 'g'), 'minus')
+    .replace(new RegExp('\\*', 'g'), 'times')
+    .replace(new RegExp('/', 'g'), 'divide')
+    .replace(new RegExp('%', 'g'), 'percent')
+    .replace(new RegExp(',', 'g'), 'comma');
+  modules[label] = '';
+  return label
+}
 
 function compile(ast, result) {
   if (Array.isArray(ast)) {
@@ -19,7 +37,11 @@ function compile(ast, result) {
     } else if (ast.type == "Identifier") {
       result.push(ast.name);
     } else if (ast.type == "ExpressionStatement") {
-      compile(ast.expression, result);
+      const stmt = []
+      compile(ast.expression, stmt);
+      stmt.push(';\n');
+      label = stmtTolabel(ast, stmt)
+      result.push(label + '(context);\n')
     } else if (ast.type == "BinaryExpression") {
       compile(ast.left, result);
       result.push(' ');
@@ -28,12 +50,16 @@ function compile(ast, result) {
       compile(ast.right, result);
     } else if (ast.type == "VariableDeclaration") {
       for (let i=0; i<ast.declarations.length; i++) {
-        result.push(ast.kind);
-        result.push(' ');
-        result.push(ast.declarations[i].id.name)
-        result.push(' = ');
-        compile(ast.declarations[i].init, result);
-        result.push(';\n');
+        const stmt = []
+        stmt.push(ast.kind);
+        stmt.push(' ');
+        stmt.push(ast.declarations[i].id.name)
+        stmt.push(' = ');
+        compile(ast.declarations[i].init, stmt);
+        stmt.push(';\n');
+        label = stmtTolabel(ast, stmt);
+        console.log('Found:' + label);
+        result.push(label + '(context);\n')
       }
     } else if (ast.type == "AssignmentExpression") {
       compile(ast.left, result);
@@ -41,13 +67,27 @@ function compile(ast, result) {
       result.push(ast.operator);
       result.push(' ');
       compile(ast.right, result);
-      result.push(';\n');
     } else {
       throw "NotImplemented " + ast.type;
     }
   }
 }
 
-result = []
-compile(ast.body, result)
+let result = []
+const modules = {}
+
+compile(ast.body, result);
+
+const header = [];
+header.push("const get = require('get');\n");
+for (i in modules) {
+  header.push("const " + i + " = require('" + i + "');\n")
+}
+header.push('\n')
+result = header.concat(result);
+
 console.log(result.join(''));
+
+if (!fs.statSync('output')) {
+  fs.mkdirSync('output')
+}
