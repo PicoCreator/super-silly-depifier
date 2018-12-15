9aacf6ef9fdf17edc397326f90b08039dd46638d
Fazli Sapuan <fazli@sapuan.org>
2018-12-15 12:32:04 +0800

    some basic shit

--- index.js
+++ index.js
@@ -5,3 +5,43 @@ filename = process.argv[2];
 
 js = fs.readFileSync(filename, 'utf-8');
 ast = Parser.parse(js);
+
+console.log(ast);
+
+function compile(ast, result) {
+  if (Array.isArray(ast)) {
+    for (let i=0; i<ast.length; i++) {
+      compile(ast[i], result);
+    }
+  } else {
+    if (ast.type == "Literal") {
+      result.push(ast.raw);
+    } else if (ast.type == "Identifier") {
+      result.push(ast.name);
+    } else if (ast.type == "BinaryExpression") {
+      compile(ast.left, result);
+      result.push(' ');
+      result.push(ast.operator);
+      result.push(' ');
+      compile(ast.right, result);
+    } else if (ast.type == "VariableDeclaration") {
+      console.dir(ast);
+      for (let i=0; i<ast.declarations.length; i++) {
+        result.push(ast.kind);
+        result.push(' ');
+        result.push(ast.declarations[i].id.name)
+        result.push(' = ');
+        compile(ast.declarations[i].init, result);
+        result.push(';\n');
+      }
+    } else if (ast.type == "ExpressionStatement") {
+      
+    } else {
+      throw "NotImplemented " + ast.type;
+    }
+  }
+}
+
+result = []
+compile(ast.body, result)
+console.log(result);
