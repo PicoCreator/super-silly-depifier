612d058010c98631328b5c24140f8983928b8952
Eugene Cheah <eugeneqin@gmail.com>
2018-12-15 13:15:40 +0800

    Extract out code to astUtil

--- index.js
+++ index.js
@@ -6,46 +6,12 @@ filename = process.argv[2];
 js = fs.readFileSync(filename, 'utf-8');
 ast = Parser.parse(js);
 
-console.log(ast);
+const astUtil = require("./src/astUtil.js");
+
+//console.log(ast);
 
 function compile(ast, result) {
-  if (Array.isArray(ast)) {
-    for (let i=0; i<ast.length; i++) {
-      compile(ast[i], result);
-    }
-  } else {
-    if (ast.type == "Literal") {
-      result.push(ast.raw);
-    } else if (ast.type == "Identifier") {
-      result.push(ast.name);
-    } else if (ast.type == "ExpressionStatement") {
-      compile(ast.expression, result);
-    } else if (ast.type == "BinaryExpression") {
-      compile(ast.left, result);
-      result.push(' ');
-      result.push(ast.operator);
-      result.push(' ');
-      compile(ast.right, result);
-    } else if (ast.type == "VariableDeclaration") {
-      for (let i=0; i<ast.declarations.length; i++) {
-        result.push(ast.kind);
-        result.push(' ');
-        result.push(ast.declarations[i].id.name)
-        result.push(' = ');
-        compile(ast.declarations[i].init, result);
-        result.push(';\n');
-      }
-    } else if (ast.type == "AssignmentExpression") {
-      compile(ast.left, result);
-      result.push(' ');
-      result.push(ast.operator);
-      result.push(' ');
-      compile(ast.right, result);
-      result.push(';\n');
-    } else {
-      throw "NotImplemented " + ast.type;
-    }
-  }
+  return astUtil.ast_to_jsStrArr(ast, result);
 }
 
 result = []
