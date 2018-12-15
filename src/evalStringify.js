0048c9c08e51ef5b7f04a5c1720bfd54aae7f083
Eugene Cheah <eugeneqin@gmail.com>
2018-12-15 14:51:36 +0800

    WIP evalStringify

---
+++ src/evalStringify.js
@@ -0,0 +1,28 @@
+/**
+ * Static Class: evalStringify
+ * 
+ * Takes in the js / ast string, and output the eval format
+ */
+
+//------------------------------------------
+//
+// Dependencies
+//
+//------------------------------------------
+
+const astUtil = require("./astUtil.js");
+const stmtUtil = require("./stmtUtil.js");
+
+//------------------------------------------
+//
+// Function implementation
+//
+//------------------------------------------
+
+function ast_to_codelineArr(ast, codelineArr) {
+
+	// Initialize codelineArr if null
+	if(codelineArr == null) {
+		codelineArr = [];
+	}
+}
