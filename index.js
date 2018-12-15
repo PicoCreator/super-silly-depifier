64df654f0330f3e7357acdc34b717dee4f820b5a
Eugene Cheah <eugeneqin@gmail.com>
2018-12-15 13:59:27 +0800

    Slow cleanup of index.js

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 const fs = require('fs');
 const Parser = require('acorn');
 
+const astUtil = require('./src/astUtil.js')
+
+
 filename = process.argv[2];
 
 js = fs.readFileSync(filename, 'utf-8');
 ast = Parser.parse(js);
 
 //console.log(ast);
-
 
 
 function stmtTolabel(ast, stmt) {
@@ -27,50 +29,7 @@ function stmtTolabel(ast, stmt) {
 }
 
 function compile(ast, result) {
-  if (Array.isArray(ast)) {
-    for (let i=0; i<ast.length; i++) {
-      compile(ast[i], result);
-    }
-  } else {
-    if (ast.type == "Literal") {
-      result.push(ast.raw);
-    } else if (ast.type == "Identifier") {
-      result.push(ast.name);
-    } else if (ast.type == "ExpressionStatement") {
-      const stmt = []
-      compile(ast.expression, stmt);
-      stmt.push(';\n');
-      label = stmtTolabel(ast, stmt)
-      result.push(label + '(context);\n')
-    } else if (ast.type == "BinaryExpression") {
-      compile(ast.left, result);
-      result.push(' ');
-      result.push(ast.operator);
-      result.push(' ');
-      compile(ast.right, result);
-    } else if (ast.type == "VariableDeclaration") {
-      for (let i=0; i<ast.declarations.length; i++) {
-        const stmt = []
-        stmt.push(ast.kind);
-        stmt.push(' ');
-        stmt.push(ast.declarations[i].id.name)
-        stmt.push(' = ');
-        compile(ast.declarations[i].init, stmt);
-        stmt.push(';\n');
-        label = stmtTolabel(ast, stmt);
-        console.log('Found:' + label);
-        result.push(label + '(context);\n')
-      }
-    } else if (ast.type == "AssignmentExpression") {
-      compile(ast.left, result);
-      result.push(' ');
-      result.push(ast.operator);
-      result.push(' ');
-      compile(ast.right, result);
-    } else {
-      throw "NotImplemented " + ast.type;
-    }
-  }
+  return astUtil.ast_to_jsStrArr(ast, result);
 }
 
 let result = []
